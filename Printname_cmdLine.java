38c93d5ab2454991bad6397ef85204f2e8c4a48f
Abhishek <you@example.com>
2022-07-01 19:29:41 +0530

    day1_assignment

---
+++ Printname_cmdLine.java
@@ -0,0 +1,15 @@
+package assignment1;
+import java.util.Scanner;
+public class Printname_cmdLine {
+public static void main(String[] args) {
+			
+	System.out.println("Enter a command line" );
+				Scanner sc = new Scanner(System.in);
+				String name = sc.nextLine();
+			
+				
+				System.out.println("You entered " +name);
+
+			}
+
+		}
